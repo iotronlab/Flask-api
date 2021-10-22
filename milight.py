66327d447ca8bac5e989ab49697a62e9829c4979
iotronlab <iotronlabspvt@gmail.com>
2021-10-22 18:24:39 +0530

    update

--- milight.py
+++ milight.py
@@ -14,3 +14,19 @@ def switch_yeelight():
     bulb = Bulb(data['ip'])
     bulb.toggle()
     return jsonify({'data': 'switched'}), 200
+
+
+@app.route('/yeelight/bright', methods=['POST'])
+def switch_brightness():
+    data = request.get_json()
+    bulb = Bulb(data['ip'])
+    bulb.set_brightness(data['bright'])
+    return jsonify({'data': 'brightness changed'}), 200
+
+
+@app.route('/yeelight/setrgb', methods=['POST'])
+def switch_color():
+    data = request.get_json()
+    bulb = Bulb(data['ip'])
+    bulb.set_rgb(data['rgb'])
+    return jsonify({'data': 'color changed'}), 200
